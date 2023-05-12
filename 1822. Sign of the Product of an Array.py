b3891ddb382325fa077575fca0f6bc38f5da33df
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-12 22:44:51 +0530

    final edit

--- 1822. Sign of the Product of an Array.py
+++ 1822. Sign of the Product of an Array.py
@@ -20,12 +20,17 @@ obj = Solution()
 # Example 1
 nums = [-1,-2,-3,-4,3,2,1]
 print(obj.arraySign(nums))
+# Expected Output : 1
 
 # Example 2
 nums = [1,5,0,2,-3] 
 print(obj.arraySign(nums))
+# Expected Output : 0
 
 #Example 3
 nums = [-1,1,-1,1,-1]
 print(obj.arraySign(nums))
+# Expected Output : -1
+
+
 

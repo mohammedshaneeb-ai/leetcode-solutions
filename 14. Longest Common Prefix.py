36e31fd901aa12f022f1fe166e2e7a6d6714842f
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-13 22:03:59 +0530

    edited

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -6,9 +6,9 @@ class Solution:
         # Sort the array of strings to simplify the logic
         strs.sort()
         
+        # Consider the first and last strings in the array
         first_str, last_str = strs[0], strs[-1]
         
-        # Find the common prefix between the first and last strings
         common_prefix = ""
         for i in range(len(first_str)):
             if first_str[i] == last_str[i]:

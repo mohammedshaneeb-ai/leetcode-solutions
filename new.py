6d82981c41c0518942f1a29d8223c0fb3b190418
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-16 22:40:28 +0530

    edited

--- new.py
+++ new.py
@@ -2,13 +2,11 @@ def searchInsert(arr,target):
     for i in range(len(arr)):
         if arr[i] == target:
             return i
-            break
     
     if target not in arr:
         for i in range(len(arr)-1):
                 if target < arr[i]:
                     return i
-                    break
                 
         return len(arr)
                      

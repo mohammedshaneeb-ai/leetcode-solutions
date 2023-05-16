88a701789859c8bb35b8ebfda4d9b35436e6fe78
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-16 22:39:50 +0530

    commited

---
+++ new.py
@@ -0,0 +1,23 @@
+def searchInsert(arr,target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+            break
+    
+    if target not in arr:
+        for i in range(len(arr)-1):
+                if target < arr[i]:
+                    return i
+                    break
+                
+        return len(arr)
+                     
+
+            
+
+
+    
+nums = [1,3,5,6]
+target = 7
+
+print(searchInsert(nums,target))

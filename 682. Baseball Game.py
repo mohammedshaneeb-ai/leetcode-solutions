346eca388d849f98485ba9468f575b59e9def204
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-17 23:33:43 +0530

    added

---
+++ 682. Baseball Game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+
+        for op in operations:
+            if op.isdigit() or (op[0] == '-' and op[1:].isdigit()):
+                
+                record.append(int(op))
+            elif op == '+':
+                
+                record.append(record[-1] + record[-2])
+            elif op == 'D':
+                
+                record.append(record[-1] * 2)
+            elif op == 'C':
+                
+                record.pop()
+
+        return sum(record)
+
+

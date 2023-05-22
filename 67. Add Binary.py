3ae3d23d3ee3ed0026966baf095c001f4a6f4529
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-22 14:05:58 +0530

    edited

--- 67. Add Binary.py
+++ 67. Add Binary.py
@@ -10,10 +10,11 @@ class Solution:
             # Get the current digits at positions i and j
             digit_a = int(a[i]) if i >= 0 else 0
             digit_b = int(b[j]) if j >= 0 else 0
-            
+
             # Calculate the sum and carry
             digit_sum = digit_a + digit_b + carry
             carry = digit_sum // 2
+            
             # Append the sum digit to the result
             result.append(str(digit_sum % 2))
             # Move to the next digits

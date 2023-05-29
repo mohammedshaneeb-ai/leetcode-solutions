a0cba6d120c11d06a81739698bfc00c80f12b7a7
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-29 14:49:07 +0530

    new added

---
+++ 69. Sqrt(x).py
@@ -0,0 +1,13 @@
+def mySqrt(x):
+        if x == 0:
+            return 0
+
+        left, right = 1, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right
+

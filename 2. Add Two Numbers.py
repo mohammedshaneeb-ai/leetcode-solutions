f01044159f2c01ed83d2127d4c1356a5e8db7c86
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-30 22:52:23 +0530

    edited

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -3,6 +3,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+        
 class Solution:
     def addTwoNumbers(self, l1, l2) :
         dummy = ListNode()

ec0483ad967c7f76510ee2a6d823ca72f8f23238
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-30 22:53:19 +0530

    example added

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -3,7 +3,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
     def addTwoNumbers(self, l1, l2) :
         dummy = ListNode()
@@ -23,3 +23,9 @@ class Solution:
             carry //= 10
 
         return dummy.next
+
+# Example 1    
+l1 = [2,4,3]
+l2 = [5,6,4]
+# Expected Output: [7,0,8]
+# Explanation: 342 + 465 = 807.

20bed8704af02510ffc77a6d6102b8bc4d12ff61
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-05-30 22:57:07 +0530

    edited

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -41,23 +41,17 @@ l2 = [9,9,9,9]
 # Expected Output: [8,9,9,9,0,0,0,1]
 
 
-# Example input: 342 + 465 = 807
-# Create the first linked list: 2 -> 4 -> 3
 l1 = ListNode(2)
 l1.next = ListNode(4)
 l1.next.next = ListNode(3)
 
-# Create the second linked list: 5 -> 6 -> 4
 l2 = ListNode(5)
 l2.next = ListNode(6)
 l2.next.next = ListNode(4)
 
-# Add the two numbers
 result = l1.addTwoNumbers(l1, l2)
 
-# Print the result
 while result:
     print(result.val, end=" ")
     result = result.next
 
-# Output: 7 0 8

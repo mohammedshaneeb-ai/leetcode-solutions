6bb3ca70ca7550f849ea69907de9b05708147d98
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-06-02 22:55:25 +0530

    edited

--- 2582. Pass the Pillow.py
+++ 2582. Pass the Pillow.py
@@ -1,5 +1,5 @@
 class Solution:
-    def passThePillow(self,n,time):
+    def passThePillow(self,n,time):          
         cycles = time //2 (2*(n-1))
         direction = 1 if cycles % 2 == 0 else -1
 		index = 1 if direction == 1 else n

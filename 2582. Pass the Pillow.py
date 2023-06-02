643580ec7c915b235697b589224ac51db987e7c8
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-06-02 22:53:16 +0530

    added

--- 2582. Pass the Pillow.py
+++ 2582. Pass the Pillow.py
@@ -3,4 +3,13 @@ class Solution:
         cycles = time //2 (2*(n-1))
         direction = 1 if cycles % 2 == 0 else -1
 		index = 1 if direction == 1 else n
+	
 		remaining_seconds = time % (2 * (n - 1))
+
+        if remaining_seconds <= n - 1:
+					index += direction * remaining_seconds
+			else:
+					index += direction * (n - 1) - direction * (remaining_seconds - (n - 1))
+			
+			if direction == -1:
+					index = n - index + 1
